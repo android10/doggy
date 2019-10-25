6e9846f14ab2172e13044e868387ffd4208c2326
Fernando Cejas <fcejas@gmail.com>
2019-10-25 17:29:01 +0200

    Using console to print and style text.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use console::Term;
+use console::style;
+
 fn main() {
-    println!("Hello, world!");
+    let term = Term::stdout();
+    let _result = term.write_line("Hello World");
+    println!("Hello {} with Style", style("Fernando").cyan());
 }
