0fe841dea73a5a61e06c6ef111150b1d13fcd927
Fernando Cejas <fcejas@gmail.com>
2019-10-27 00:02:31 +0200

    Execute one command succesfully.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,49 @@
 use console::Term;
 use console::style;
+use std::io::*;
+use std::process::Command;
 
 fn main() {
-    let term = Term::stdout();
-    let _result = term.write_line("Hello World");
-    menu();
-    let _option = term.read_char();
+    print_main_menu();
+    read_option(Term::stdout().read_line());
 }
 
-fn menu() {
+fn print_main_menu() {
     println!("---------------------------------------------");
     println!("Welcome to {} - Version 1.0.0", style("Doggy").green());
     println!("---------------------------------------------");
     println!(" 1 - {}", style("Check pacman cache size.").blue());
     println!(" 2 - {}", style("Check pacaur cache size.").cyan());
     println!("---------------------------------------------");
-    println!("Choose one option: ");
-    println!("---------------------------------------------");
+    println!("Your option: ");
+}
+
+fn read_option(result: Result<String>) {
+    match result {
+        Ok(opt) => process_option(opt.as_str()),
+        Err(e)  => println!("Error: {}", e),
+    }
+}
+
+fn process_option(opt: &str) {
+    match opt {
+        "1" => execute_command("du", "-sh /var/cache/pacman/pkg"),
+        "2" => execute_command("du", "-sh ~/.cache/pacaur/"),
+        _ => println!("Any other option pressed"),
+    }
+}
+
+fn execute_command(_cmd: &str, _arg: &str) {
+    let output = Command::new("du")
+                         .arg("-s")
+                         .arg("-h")
+                         .arg("/var/cache/pacman/pkg")
+                         .output()
+                         .expect("failed to execute process");
+
+    println!("status: {}", output.status);
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+    assert!(output.status.success());
 }
