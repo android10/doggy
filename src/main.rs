623330300af2fd89dd835957dfa229cfa37da3ca
Fernando Cejas <fcejas@gmail.com>
2019-10-26 14:37:00 +0200

    Trying to draw a simple menu.

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,17 @@ use console::style;
 fn main() {
     let term = Term::stdout();
     let _result = term.write_line("Hello World");
-    println!("Hello {} with Style", style("Fernando").cyan());
+    menu();
+    let _option = term.read_char();
+}
+
+fn menu() {
+    println!("---------------------------------------------");
+    println!("Welcome to {} - Version 1.0.0", style("Doggy").green());
+    println!("---------------------------------------------");
+    println!(" 1 - {}", style("Check pacman cache size.").blue());
+    println!(" 2 - {}", style("Check pacaur cache size.").cyan());
+    println!("---------------------------------------------");
+    println!("Choose one option: ");
+    println!("---------------------------------------------");
 }
